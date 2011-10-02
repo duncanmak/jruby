bee6217569356219502087d41831774a78de74b6
Duncan Mak <duncan@earthaid.net>
2011-10-02 02:22:14 -0400

    Implement #afamily.

--- src/org/jruby/ext/socket/Addrinfo.java
+++ src/org/jruby/ext/socket/Addrinfo.java
@@ -12,7 +12,7 @@
  * rights and limitations under the License.
  *
  * Copyright (C) 2011 Duncan Mak <duncan@earthaid.net>
- * 
+ *
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
  * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
@@ -27,16 +27,29 @@
  ***** END LICENSE BLOCK *****/
 package org.jruby.ext.socket;
 
+import static jnr.constants.platform.AddressFamily.AF_INET;
+import static jnr.constants.platform.AddressFamily.AF_INET6;
+import static jnr.constants.platform.IPProto.IPPROTO_TCP;
+import static jnr.constants.platform.IPProto.IPPROTO_UDP;
+import static jnr.constants.platform.NameInfo.NI_NUMERICHOST;
+import static jnr.constants.platform.NameInfo.NI_NUMERICSERV;
+import static jnr.constants.platform.ProtocolFamily.PF_INET;
+import static jnr.constants.platform.ProtocolFamily.PF_INET6;
+import static jnr.constants.platform.Sock.SOCK_DGRAM;
+import static jnr.constants.platform.Sock.SOCK_STREAM;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import org.jruby.Ruby;
 import org.jruby.RubyClass;
 import org.jruby.RubyObject;
+import org.jruby.RubyNumeric;
 import org.jruby.anno.JRubyClass;
 import org.jruby.anno.JRubyMethod;
 import org.jruby.exceptions.RaiseException;
 import org.jruby.runtime.ObjectAllocator;
+import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
 
 @JRubyClass(name="Addrinfo")
@@ -53,25 +66,32 @@ public class Addrinfo extends RubyObject {
         result.setReifiedClass(Addrinfo.class);
         result.defineAnnotatedMethods(Addrinfo.class);
     }
-    
+
     public Addrinfo(Ruby runtime, RubyClass klass) {
         super(runtime, klass);
     }
 
-
-    public Addrinfo(Ruby runtime, InetAddress address) {
+    public Addrinfo(Ruby runtime, InetAddress address, int port, int family, int socket_type, int protocol) {
         super(runtime, runtime.getClass("Addrinfo"));
         this.address = address;
+        this.port = port;
+        this.family = family;
+        this.socket_type = socket_type;
+        this.protocol = protocol;
     }
 
     private InetAddress address;
+    private int port;
+    private int family;
+    private int socket_type;
+    private int protocol;
 
     @JRubyMethod(meta = true)
     public static IRubyObject ip(IRubyObject self, IRubyObject arg) {
         Ruby runtime = self.getRuntime();
         try {
             String host = arg.convertToString().toString();
-            return new Addrinfo(runtime, InetAddress.getByName(host));
+            return new Addrinfo(runtime, InetAddress.getByName(host), 0, AF_INET.intValue(), 0, 0);
         } catch (UnknownHostException e) {
             throw new RaiseException(
                 runtime, runtime.getClass("SocketError"), "getaddrinfo: Name or service not known", true);
@@ -87,9 +107,14 @@ public class Addrinfo extends RubyObject {
     public static IRubyObject udp(IRubyObject self, IRubyObject host, IRubyObject port) {
         throw new UnsupportedOperationException();
     }
-    
+
     @JRubyMethod(meta = true, required = 1, optional = 1)
     public static IRubyObject unix(IRubyObject recv, IRubyObject[] args) {
         throw new UnsupportedOperationException();
+    }
+
+    @JRubyMethod
+    public RubyNumeric afamily(ThreadContext ctx) {
+        return RubyNumeric.int2fix(ctx.getRuntime(), family);
     }
 }
